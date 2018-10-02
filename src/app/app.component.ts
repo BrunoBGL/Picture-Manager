d542cca0096e895c292597d76ab0d4cc26e54ef3
Bruno Gustavo de Lima <bruno.glima@spread.com.br>
2018-10-02 15:59:21 -0300

    L02 - Components, Directives, Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
-  description ='Descrição da Imagem';
-  link = 'https://kids.nationalgeographic.com/content/dam/kids/photos/games/screen-shots/More%20Games/A-G/babyanimal_open.ngsversion.1429194155981.jpg';
+
+  photos = [
+    {
+      url: "https://s2.glbimg.com/vEQUudcmd1LgW5tI4Oi3R2Z_wsw=/620x413/e.glbimg.com/og/ed/f/original/2015/07/30/dsc9081.jpg", 
+      description: "Subaru Impreza"},
+    {
+      url: "https://www.caranddriver.com/images/17q1/674191/2018-jaguar-xe-updated-with-new-engine-options-news-car-and-driver-photo-675945-s-original.jpg",
+      description:"Jaguar XE"
+    }
+  ]
+ 
 }
