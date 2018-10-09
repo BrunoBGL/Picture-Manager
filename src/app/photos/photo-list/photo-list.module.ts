89ecfdcb369b2a64ce5667c69cad203a89b4954b
Bruno Gustavo de Lima <bruno.glima@spread.com.br>
2018-10-09 15:43:39 -0300

    L07 - Directives, Outbound Propertty

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescription } from './filter-by-description.pipe';
+import { CommonModule } from '@angular/common';
+import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from '../../shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverDirective } from '../../directives/darken-on-hover/darken-on-hover.directive';
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescription,
+        SearchComponent,
+        DarkenOnHoverDirective
+    ],
+    imports:[
+        CommonModule, 
+        PhotoModule, 
+        CardModule
+    ]
+})
+export class PhotoListModule{
+
+}
